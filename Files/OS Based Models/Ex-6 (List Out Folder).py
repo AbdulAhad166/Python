e98d0f37c04f112f39636198c5bfc56acc67affc
ABDUL AHAD <abdulahad162005@gmail.com>
2026-08-05 17:07:52 +0530

    Initial Commit

---
+++ Files/OS Based Models/Ex-6 (List Out Folder).py
@@ -0,0 +1,10 @@
+#Program for Listing the Folder Data Present in folder in file---listdir()
+import os
+try:
+    FolderName=input("Enter Folder Name in List Files: ")
+    filelist=os.listdir(FolderName)
+    print("List of Files: ")
+    for filename in filelist:
+        print(filename)
+except FileNotFoundError:
+    print("Folder Does Not Exist")
