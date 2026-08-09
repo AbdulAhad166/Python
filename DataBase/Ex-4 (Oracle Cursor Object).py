e29d41ed9cca87a98e2e6a2835793ca5f8ebce3d
ABDUL AHAD <abdulahad162005@gmail.com>
2026-08-09 23:28:57 +0530

    Update Ex-4 (Oracle Cursor Object).py

--- DataBase/Ex-4 (Oracle Cursor Object).py
+++ DataBase/Ex-4 (Oracle Cursor Object).py
@@ -1,5 +1,5 @@
 #Program for Creating Cursor Object
-import oracledb as orc
+import oracledb as orc. #step-1
 conobj=orc.connect("system/ahad@localhost/free")
 print("Python Program Got Connection From Oracledb")
 print("type of conobj= ",type(conobj))
