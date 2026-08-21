7daf8ecd38155093778f66d87f50e7dc8759bfa6
ABDUL AHAD <abdulahad162005@gmail.com>
2026-08-21 17:32:00 +0530

    Initial-Commit

--- OOPS/Polymorphism/Ex-3 (Multi-Level Polymorphism).py
+++ OOPS/Polymorphism/Ex-3 (Multi-Level Polymorphism).py
@@ -1,15 +1,15 @@
 #Program for Using Polymorphism --- Multi-Level Inheritance
 class Rectangle:
-    def draw(self):
+    def draw(self):            #Original Method
         print("Drawing Rectangle")
 class Circle(Rectangle):
-    def draw(self):
+    def draw(self):           #Method Overridden
         print("Drawing Circle")
 class Square(Circle):
-    def draw(self):
+    def draw(self):              #Method Overridden
         print("Drawing Square")
-        Rectangle.draw(self)
-        Circle.draw(self)
+        Rectangle.draw(self)  #Class Name Approach
+        Circle.draw(self)     #Class Name Approach
 #Main Program
 s=Square()
 s.draw()
